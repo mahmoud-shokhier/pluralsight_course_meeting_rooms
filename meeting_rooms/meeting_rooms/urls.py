29615fa10fd28cdddff102408d109740fcb95585
mahmoud-shokhier <mahmoud.aly.shokhier@gmail.com>
2021-12-25 16:56:43 +0200

    add the first routes in the app, welcome route

--- meeting_rooms/meeting_rooms/urls.py
+++ meeting_rooms/meeting_rooms/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from website.views import welcome
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('welcome', welcome)
 ]
